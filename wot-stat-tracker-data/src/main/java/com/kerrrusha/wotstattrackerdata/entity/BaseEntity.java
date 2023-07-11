b22fddd8ba9110cf696f83c4e85542dd9a887db8
Kirill Koval <brcyrill2@gmail.com>
2023-07-11 18:44:47 +0300

    fixed default current timestamp

--- wot-stat-tracker-data/src/main/java/com/kerrrusha/wotstattrackerdata/entity/BaseEntity.java
+++ wot-stat-tracker-data/src/main/java/com/kerrrusha/wotstattrackerdata/entity/BaseEntity.java
@@ -1,14 +1,10 @@
 package com.kerrrusha.wotstattrackerdata.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -23,7 +19,7 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @CreationTimestamp
+    @Column(name="created_at", columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createdAt;
 
 }
