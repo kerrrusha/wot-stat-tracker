bf8505524b5f9c714d35f714e7c001df0c52c013
Kirill Koval <brcyrill2@gmail.com>
2023-07-15 20:54:41 +0300

    refactored

--- wot-stat-tracker-web/src/test/java/com/kerrrusha/wotstattrackerweb/repository/StatRepositoryIT.java
+++ wot-stat-tracker-web/src/test/java/com/kerrrusha/wotstattrackerweb/repository/StatRepositoryIT.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -25,7 +27,8 @@ class StatRepositoryIT {
 
     @Test
     void findAllByPlayerNickname() {
-        List<Stat> stats = statRepository.findAllByPlayerNicknameOrderByCreatedAtDesc(NICKNAME);
+        Pageable pageable = PageRequest.of(0, 15);
+        List<Stat> stats = statRepository.findDistinctByPlayer_NicknameLikeOrderByCreatedAtDesc(NICKNAME, pageable);
         log.info("Found {} stats for {} nickname", stats.size(), NICKNAME);
         stats.forEach(stat -> log.info(stat.getLastBattleTime().toString()));
     }
