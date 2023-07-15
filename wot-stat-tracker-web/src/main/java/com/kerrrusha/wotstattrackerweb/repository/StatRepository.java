457ac765ec67ce717db7535610c1e05281c17c96
Kirill Koval <brcyrill2@gmail.com>
2023-07-15 18:43:04 +0300

    limited query results

--- wot-stat-tracker-web/src/main/java/com/kerrrusha/wotstattrackerweb/repository/StatRepository.java
+++ wot-stat-tracker-web/src/main/java/com/kerrrusha/wotstattrackerweb/repository/StatRepository.java
@@ -1,13 +1,14 @@
 package com.kerrrusha.wotstattrackerweb.repository;
 
 import com.kerrrusha.wotstattrackerweb.entity.Stat;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface StatRepository extends JpaRepository<Stat, Long> {
 
-    List<Stat> findAllByPlayerNicknameOrderByCreatedAtDesc(String nickname);
+    List<Stat> findDistinctByPlayer_NicknameLikeOrderByCreatedAtDesc(String nickname, Pageable pageable);
 
     Stat findFirstByPlayer_NicknameLikeOrderByCreatedAtDesc(String nickname);
 
