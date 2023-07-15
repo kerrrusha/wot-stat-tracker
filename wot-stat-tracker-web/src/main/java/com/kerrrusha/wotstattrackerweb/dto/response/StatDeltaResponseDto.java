ca5e06713e151af30b104e33f917fcff145f6263
Kirill Koval <brcyrill2@gmail.com>
2023-07-15 04:08:01 +0300

    fixed delta percent bug

--- wot-stat-tracker-web/src/main/java/com/kerrrusha/wotstattrackerweb/dto/response/StatDeltaResponseDto.java
+++ wot-stat-tracker-web/src/main/java/com/kerrrusha/wotstattrackerweb/dto/response/StatDeltaResponseDto.java
@@ -3,8 +3,10 @@ package com.kerrrusha.wotstattrackerweb.dto.response;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.Locale;
 
 import static com.kerrrusha.wotstattrackerweb.dto.response.StatResponseDto.formatter;
+import static org.thymeleaf.util.NumberUtils.formatPercent;
 
 @Data
 public class StatDeltaResponseDto {
@@ -12,6 +14,7 @@ public class StatDeltaResponseDto {
     private Integer battlesDelta;
     private Double avgDamageDelta;
     private Integer ratingDelta;
+
     private String winrateDeltaFormatted;
     private Double avgExperienceDelta;
     private Double wn7Delta;
@@ -19,6 +22,12 @@ public class StatDeltaResponseDto {
     private Integer treesCutDelta;
 
     private LocalDateTime previousStatCreationTime;
+
+    public void setWinrateDeltaFormatted(Double delta) {
+        this.winrateDeltaFormatted = delta == 0
+                ? "-"
+                : formatPercent(delta, 1, 4, Locale.getDefault());
+    }
 
     public String getPreviousStatCreationTimeFormatted() {
         return previousStatCreationTime.format(formatter);
