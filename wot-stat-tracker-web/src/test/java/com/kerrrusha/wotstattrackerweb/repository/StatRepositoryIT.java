3f4ab01fa77e8b2efdf9813046c4e3432a796eeb
Kirill Koval <brcyrill2@gmail.com>
2023-07-29 20:52:59 +0300

    added tests

--- wot-stat-tracker-web/src/test/java/com/kerrrusha/wotstattrackerweb/repository/StatRepositoryIT.java
+++ wot-stat-tracker-web/src/test/java/com/kerrrusha/wotstattrackerweb/repository/StatRepositoryIT.java
@@ -1,28 +1,58 @@
 package com.kerrrusha.wotstattrackerweb.repository;
 
+import com.kerrrusha.wotstattrackerweb.entity.Player;
 import com.kerrrusha.wotstattrackerweb.entity.Stat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 @SpringBootTest
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 class StatRepositoryIT {
 
-    private static final String NICKNAME = "He_Cm0Tpu_CTaTucTuKy";
+    static final String NICKNAME = "Jove";
+
     final StatRepository statRepository;
+    final PlayerRepository playerRepository;
+
+    Player examplePlayer;
+    Stat exampleStat1;
+    Stat exampleStat2;
+
+    @BeforeEach
+    void setUp() {
+        examplePlayer = Player.builder().nickname(NICKNAME).accountId("12345").build();
+
+        exampleStat1 = Stat.builder().player(examplePlayer).createdAt(LocalDateTime.now().minus(5, ChronoUnit.MINUTES)).build();
+        exampleStat2 = Stat.builder().player(examplePlayer).createdAt(LocalDateTime.now()).build();
+    }
 
     @Test
+    @Rollback
+    @Transactional
     void findFirstByPlayer_NicknameLikeOrderByCreatedAtDesc() {
-        Stat stat = statRepository.findFirstByPlayer_NicknameLikeOrderByCreatedAtDesc(NICKNAME);
-        log.info(stat.getLastBattleTime().toString());
+        playerRepository.save(examplePlayer);
+        statRepository.save(exampleStat1);
+        statRepository.save(exampleStat2);
+
+        Stat actual = statRepository.findFirstByPlayer_NicknameLikeOrderByCreatedAtDesc(NICKNAME);
+
+        assertEquals(exampleStat2, actual);
+        assertEquals(exampleStat2.getCreatedAt(), actual.getCreatedAt());
     }
 
     @Test
@@ -30,7 +60,7 @@ class StatRepositoryIT {
         Pageable pageable = PageRequest.of(0, 15);
         List<Stat> stats = statRepository.findDistinctByPlayer_NicknameLikeOrderByCreatedAtDesc(NICKNAME, pageable);
         log.info("Found {} stats for {} nickname", stats.size(), NICKNAME);
-        stats.forEach(stat -> log.info(stat.getLastBattleTime().toString()));
+        stats.forEach(stat -> log.info(stat.toString()));
     }
 
 }
---
+++ wot-stat-tracker-web/src/test/java/com/kerrrusha/wotstattrackerweb/service/impl/StatServiceImplTest.java
@@ -0,0 +1,68 @@
+package com.kerrrusha.wotstattrackerweb.service.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kerrrusha.wotstattrackerweb.entity.Stat;
+import com.kerrrusha.wotstattrackerweb.repository.StatRepository;
+import com.kerrrusha.wotstattrackerweb.service.StatService;
+import com.kerrrusha.wotstattrackerweb.service.mapper.PlayerMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jms.core.JmsTemplate;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class StatServiceImplTest {
+
+    final PlayerMapper playerMapper = new PlayerMapper();
+    final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Mock
+    JmsTemplate jmsTemplate;
+
+    @Mock
+    StatRepository statRepository;
+
+    StatService statService;
+
+    Stat stat1;
+    Stat stat2;
+
+    @BeforeEach
+    void setUp() {
+        stat1 = Stat.builder().id(1L).battles(10).build();
+        stat2 = Stat.builder().id(2L).battles(20).build();
+
+        statService = new StatServiceImpl(jmsTemplate, objectMapper, statRepository, playerMapper);
+    }
+
+    @Test
+    void findCurrentStatByNickname() {
+        when(statRepository.findFirstByPlayer_NicknameLikeOrderByCreatedAtDesc(anyString())).thenReturn(stat1);
+
+        Stat actual = statService.findCurrentStatByNickname("any");
+
+        assertEquals(actual, stat1);
+    }
+
+    @Test
+    @Disabled("Can't wire @Value private fields...")
+    void findPreviousStatByNickname() {
+        when(statRepository.findDistinctByPlayer_NicknameLikeOrderByCreatedAtDesc(anyString(), any())).thenReturn(List.of(stat1, stat2));
+
+        Optional<Stat> actual = statService.findPreviousStatByNickname("any");
+
+        assertEquals(actual.orElseThrow(), stat2);
+    }
+
+}
