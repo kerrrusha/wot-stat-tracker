34188ef6d147c80865a5c0ef30538bb76a932f0c
Kirill Koval <brcyrill2@gmail.com>
2023-08-02 02:31:53 +0300

    fixed bug

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -9,7 +9,7 @@ scp -i ~/.ssh/id-rsa-volia \
 
 echo 'Uploading wot-stat-tracker-provider...'
 scp -i ~/.ssh/id-rsa-volia \
-    ./wot-stat-tracker-provider/target/wot-stat-provider-0.0.1-SNAPSHOT.jar \
+    ./wot-stat-tracker-provider/target/wot-stat-tracker-provider-0.0.1-SNAPSHOT.jar \
     admin@82.144.223.35:/opt/kerrrusha/wot-stat-tracker
 
 echo 'Uploading wot-stat-tracker-web...'
