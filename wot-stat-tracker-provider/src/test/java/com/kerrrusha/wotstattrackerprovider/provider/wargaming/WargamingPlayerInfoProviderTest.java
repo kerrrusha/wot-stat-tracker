2019f691cae01caba7ac93f8f1109d8e23f8887a
Kirill Koval <brcyrill2@gmail.com>
2023-08-04 04:52:42 +0300

    added test

--- wot-stat-tracker-provider/src/test/java/com/kerrrusha/wotstattrackerprovider/provider/wargaming/WargamingPlayerInfoProviderTest.java
+++ wot-stat-tracker-provider/src/test/java/com/kerrrusha/wotstattrackerprovider/provider/wargaming/WargamingPlayerInfoProviderTest.java
@@ -1,32 +1,43 @@
 package com.kerrrusha.wotstattrackerprovider.provider.wargaming;
 
-import com.kerrrusha.wotstattrackerprovider.dto.wargaming.WargamingPlayersDto;
-import org.junit.jupiter.api.Disabled;
+import com.kerrrusha.wotstattrackerprovider.dto.wargaming.WargamingPlayerInfoDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-class WargamingPlayersProviderTest {
+class WargamingPlayerInfoProviderTest {
 
     @Autowired
-    WargamingPlayersProvider provider;
+    WargamingPlayerInfoProvider provider;
 
     @Test
-    @Disabled
     void testMyNickname() {
         String nickname = "He_Cm0Tpu_CTaTucTuKy";
 
-        WargamingPlayersDto responseDto = provider.findByNickname(nickname);
+        WargamingPlayerInfoDto responseDto = provider.findByNickname(nickname);
 
         assertNotNull(responseDto);
         assertNotNull(responseDto.getAccountId());
         assertEquals(nickname, responseDto.getNickname());
+    }
+
+    @Test
+    void testInvalidNickname() {
+        String nickname = "::::::::::";
+
+        WargamingPlayerInfoDto responseDto = provider.findByNickname(nickname);
+
+        assertNotNull(responseDto);
+        assertNotNull(responseDto.getError());
+        assertTrue(isBlank(responseDto.getNickname()));
+        assertTrue(isBlank(responseDto.getAccountId()));
     }
 
 }
