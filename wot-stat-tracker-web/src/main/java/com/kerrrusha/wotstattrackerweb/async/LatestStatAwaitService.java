f7caedace9919bab03814cfce9d5d440abe20eab
Kirill Koval <brcyrill2@gmail.com>
2023-08-05 23:51:08 +0300

    added await service

---
+++ wot-stat-tracker-web/src/main/java/com/kerrrusha/wotstattrackerweb/async/LatestStatAwaitService.java
@@ -0,0 +1,46 @@
+package com.kerrrusha.wotstattrackerweb.async;
+
+import com.kerrrusha.wotstattrackerweb.dto.request.PlayerRequestDto;
+import com.kerrrusha.wotstattrackerweb.entity.Stat;
+import com.kerrrusha.wotstattrackerweb.repository.StatRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static java.util.Objects.isNull;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LatestStatAwaitService extends AbstractSingleThreadAwaitService<PlayerRequestDto, Stat> {
+
+    private final StatRepository statRepository;
+
+    @Value("${data.update.cache.expiration.minutes}")
+    private Integer cacheExpirationMinutes;
+
+    @Override
+    protected Optional<Stat> lookupForResult(PlayerRequestDto input) {
+        Stat stat = findCurrentStatByNickname(input);
+
+        if (isNull(stat)) {
+            return Optional.empty();
+        }
+        LocalDateTime acceptedFromTime = LocalDateTime.now().minusMinutes(cacheExpirationMinutes);
+        LocalDateTime lastStatTime = stat.getCreatedAt();
+        log.debug("Accepted from time: {}", acceptedFromTime);
+        log.debug("Last stat time: {}", lastStatTime);
+        return lastStatTime.isAfter(acceptedFromTime)
+                ? Optional.of(stat)
+                : Optional.empty();
+    }
+
+    private Stat findCurrentStatByNickname(PlayerRequestDto input) {
+        return statRepository.findFirstByPlayer_NicknameAndPlayer_RegionLikeOrderByCreatedAtDesc(input.getNickname(), input.getRegion());
+    }
+
+}
